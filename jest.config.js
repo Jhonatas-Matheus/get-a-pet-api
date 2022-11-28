a61d560d8c3e5cf915b5edf2d1b1bebda1d5642e
Jhonatas <jhonatasoliveira021@gmail.com>
2022-11-28 06:39:33 -0300

    chore: add jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   collectCoverage: true,
-
   collectCoverageFrom: ["<rootDir>/src/**/*.js"],
-
+  coverageProvider: "v8",
   coverageDirectory: "coverage",
   testEnvironment: "node",
 };

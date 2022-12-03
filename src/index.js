ec1ddb041d09bc57ce04c145429e4b9ff4e3a2af
Jhonatas <jhonatasoliveira021@gmail.com>
2022-12-03 08:47:49 -0300

    chore: config cors and public folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const routeUser = require("./routes/user.routes");
 
 const app = express();
 app.use(express.json());
 app.use("/user", routeUser);
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.static("public"));
 app.listen(2580);
 
 module.exports = app;
